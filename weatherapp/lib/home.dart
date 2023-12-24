8c1840901889791be5307bcb3cf1afc9868e90c1
Fahim <fahimshahariar.fs@gmail.com>
2023-12-24 18:57:06 +0600

    2

--- weatherapp/lib/home.dart
+++ weatherapp/lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,7 +12,17 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+
+    var date = DateFormat("yMMMMd").format(DateTime.now());
+
     return Scaffold(
+      appBar: AppBar(
+        title: "$date".text.gray700.make(),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.light_mode, color: Vx.gray600,)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.more_vert, color: Vx.gray600,))
+        ],
+      ),
       body: Column(),
     );
 
--- weatherapp/lib/main.dart
+++ weatherapp/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:weatherapp/home.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 void main() {
   runApp(const MyApp());
